6ada9f5217ce5b67ea729bf5b864280f4eb2d3ad
Elly <ellyjames1999@gmail.com>
2024-12-04 16:39:09 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// code your solution 
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(record){
+    let x = record.find((game)=> game.result==="W")
+    if(x){
+        return x.year
+    }else{
+        return undefined;
+    }
+    
+
+   
+}
+
+console.log(superbowlWin(record));
